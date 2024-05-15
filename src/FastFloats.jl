5e9e69b81e164735e2d9562435d07155b513585e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-05-14 23:58:26 -0400

    Update and rename DesignerFloats.jl to FastFloats.jl

--- src/FastFloats.jl
+++ src/FastFloats.jl
@@ -1,11 +1,21 @@
-module DesignerFloats
+module FastFloats
+
+export BinaryFloat, SignedFloat, UnsignedFloat
 
 using BitOperations                     # bitfields: mask, shift, unmask
 using BitIntegers, Quadmath             # Rational{Int1024}, Float128
 using Printf                            # hexadecimal strings
 using CSV, Tables, DataFrames           # tables in memory and in files
 
+include("type/abstraction.jl")
+include("type/realization.jl")
 
+include("qualia/counts.jl")
+include("qualia/extrema.jl")
+include("qualia/magnitudes.jl")
+
+include("provide/values.jl")
+include("provide/tables.jl")
 
 end  # DesignerFloats
 
