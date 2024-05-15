761f32d1b8ea5e8b36733078a9b83d32233e3e3e
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-05-15 01:26:22 -0400

    comment

--- src/type/realization.jl
+++ src/type/realization.jl
@@ -72,7 +72,7 @@ Base.@kwdef struct SignedFloat{W,P} <: BinaryFloat{W,P}
 
     function SignedFloat{W,P}(inf::Bool, nan::Bool) where {W,P}
         @assert (W > P) && (P > 0)
-        inf && @assert (W > 2)
+        inf && @assert (W > 2)  # W==2 -> {-Inf, 0, NaN, +Inf}
         new{W,P}(inf, nan)
     end
 end
