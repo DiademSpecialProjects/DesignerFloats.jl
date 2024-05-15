e12431e18669d1768ef3af9ba776854e667d2565
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-05-15 13:04:42 -0400

    extrema

--- src/qualia/extrema.jl
+++ src/qualia/extrema.jl
@@ -4,56 +4,40 @@
         signficands
 =#
 
+function simple_values(minval, maxval)
+    (isnothing(minval) && isnothing(maxval)) && return NoValues
+    isnothing(maxval)  && return ValType[minval]
+    isnothing(minval)  && return ValType[maxval]
+    
+    if isequal(minval, maxval)
+        ValType[minval]
+    else
+        ValType[mival, maxval]
+    end
+end
+
 function extremal_subnormal_exponents(x::BinaryFloat{W,P}) where {W,P}
     n = n_subnormal_exponents(x)
     iszero(n) && return NoValues
-    isone(n)  && return ValTypes[min_subnormal_exponent(x)]
-
-    mn, mx = min_subnormal_exponent(x), max_subnormal_exponent(x)
-    if isequal(mn, mx)
-        ValType[mn]
-    else
-        ValType[mn, mx]
-    end
+    simple_values(min_subnormal_exponent(x), max_subnormal_exponent(x))
 end
 
 function extremal_normal_exponents(x::BinaryFloat{W,P}) where {W,P}
     n = n_normal_exponents(x)
     iszero(n) && return NoValues
-    isone(n) && return ValTypes[min_normal_exponent(x)]
-
-    mn, mx = min_normal_exponent(x), max_normal_exponent(x)
-    if isequal(mn, mx)
-        ValType[mn]
-    else
-        ValType[mn, mx]
-    end
+    simple_values(min_normal_exponent(x), max_normal_exponent(x))
 end
 
 function extremal_subnormal_significands(x::BinaryFloat{W,P}) where {W,P}
     n = n_subnormal_significands(x)
     iszero(n) && return NoValues
-    isone(n) && return ValTypes[min_subnormal_significand(x)]
-
-    mn, mx = min_subnormal_significand(x), max_subnormal_significand(x)
-    if isequal(mn, mx)
-        ValType[mn]
-    else
-        ValType[mn, mx]
-    end
+    simple_values(min_subnormal_significand(x), max_subnormal_significand(x))
 end
 
 function extremal_normal_significands(x::BinaryFloat{W,P}) where {W,P}
     n = n_normal_significands(x)
     iszero(n) && return NoValues
-    isone(n) && return ValTypes[min_normal_significand(x)]
-
-    mn, mx = min_normal_significand(x), max_normal_significand(x)
-    if isequal(mn, mx)
-        ValType[mn]
-    else
-        ValType[mn, mx]
-    end
+    simple_values(min_normal_significand(x), max_normal_significand(x))
 end
 
 function min_subnormal_exponent(x::BinaryFloat{W,P}) where {W,P}
@@ -89,7 +73,7 @@ end
 function max_subnormal_significand(x::BinaryFloat{W,P}) where {W,P}
     n = n_subnormal_significands(x)
     iszero(n) && return nothing
-    RationalNK(n, n+1)
+    RationalNK(CountSignificances(x) - 1, CountSignificances(x))
 end
 
 function min_normal_significand(x::BinaryFloat{W,P}) where {W,P}
