4a37a7e71dbda555023c006d215806b04af431b1
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-05-16 00:33:58 -0400

    correct

--- src/qualia/magnitudes.jl
+++ src/qualia/magnitudes.jl
@@ -7,8 +7,8 @@ end
 function subnormal_magnitudes(x::BinaryFloat{W,P}) where {W,P}
     n = n_subnormal_magnitudes(x)
     iszero(n) && return copy(NoValues)
-    xps = range(subnormal_exponent_values(x)..., length=n_subnormal_exponents(x))
-    sgs = range(subnormal_significand_values(x)..., length=n_subnormal_signficands(x))
+    xps = range(extremal_subnormal_exponent_values(x)..., length=n_subnormal_exponents(x))
+    sgs = range(extremal_subnormal_significand_values(x)..., length=n_subnormal_signficands(x))
     mags = copy(NoValues)
     for xp in xps
         for sg in sgs
@@ -21,8 +21,8 @@ end
 function normal_magnitudes(x::BinaryFloat{W,P}) where {W,P}
     n = n_normal_magnitudes(x)
     iszero(n) && return copy(NoValues)
-    xps = range(normal_exponent_values(x)..., length=n_normal_exponents(x))
-    sgs = range(normal_significand_values(x)..., length=n_normal_signficands(x))
+    xps = range(extremal_normal_exponent_values(x)..., length=n_normal_exponents(x))
+    sgs = range(extremal_normal_significand_values(x)..., length=n_normal_signficands(x))
     mags = copy(NoValues)
     for xp in xps
         for sg in sgs
