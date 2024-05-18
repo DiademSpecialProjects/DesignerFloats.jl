7d20625404552d5d69648a7804455145e6f0333c
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-05-18 16:03:23 -0400

    table header

--- src/provide/tables.jl
+++ src/provide/tables.jl
@@ -1,10 +1,18 @@
 function prettytable(x::BinaryFloat{W,P}) where {W,P}
-    vecs= [encodings_signs_exponents_significands_values(x)...]
-    df = DataFrame(vecs, [:encoding, :sign, :exponent, :significand, :value])
+    vecs = [encodings_signs_exponents_significands_values(x)...]
+    header = [:encoding, :sign, :exponent, :significand, :value]
+    df = DataFrame(vecs, header)
+    arr = Any[]
+    mp = (map(x -> Any[x...], map(values, rowtable(df))))
+    for row in mp
+        push!(arr, row...)
+    end
+    data = collect(transpose(reshape(arr, 5,n_values(x))))
 
     fmt1(v,i,j)=(j==1 ? string("0x",@sprintf("%02x",v)) : v)
     fmt3(v,i,j)=(j>=3 && isa(v,Rational) ? string(numerator(v),"/",denominator(v)) : v)
 
-    pretty_table(df, formatters=(fmt1,fmt3))
+    pretty_table(data; formatters=(fmt1,fmt3), header=header)
 end
+
 
