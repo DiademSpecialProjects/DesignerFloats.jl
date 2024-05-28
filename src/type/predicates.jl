34051778765539f2ba460beac94d0777c600169d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-05-28 01:32:04 -0400

    Update predicates.jl

--- src/type/predicates.jl
+++ src/type/predicates.jl
@@ -6,9 +6,9 @@ is_unsigned(::Type{T}) where {T<:BinaryFloat} = !is_signed(T)
 is_unsigned(x::T) where {T<:BinaryFloat} = !is_signed(T)
       
 is_finite(::Type{T}) where {T<:FiniteSignedFloat} = true
-is_finite(::Type{T}) where {T<:FiniteUnsigedFloat} = true
+is_finite(::Type{T}) where {T<:FiniteUnsignedFloat} = true
 is_finite(::Type{T}) where {T<:SignedFloat} = false
-is_finite(::Type{T}) where {T<:UnsigedFloat} = false
+is_finite(::Type{T}) where {T<:UnsignedFloat} = false
 is_finite(X::T) where {T<:BinaryFloat} = is_finite(T)
 
 has_infinity(::Type{T}) where {T<:BinaryFloat} = !is_finite(T)
