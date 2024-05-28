e18aee3a142dc7f5db221a378b4aa61bf191e39d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-05-27 22:00:28 -0400

    Update predicates.jl

--- src/type/predicates.jl
+++ src/type/predicates.jl
@@ -1,9 +1,1 @@
-is_signed(x::UnsignedFLOAT{W,P}) where {W,P} = false
-is_signed(x::SignedFLOAT{W,P}) where {W,P} = true
-
-is_unsigned(x::UnsignedFLOAT{W,P}) where {W,P} = true
-is_unsigned(x::SignedFLOAT{W,P}) where {W,P} = false
-
-has_subnormal_values(x::BinaryFLOAT{W,P}) where {W,P} = !iszero(n_subnormal_magnitudes(x))
-has_normal_values(x::SignedFLOAT{W,P}) where {W,P} = !iszero(n_normal_magnitudes(x))
 
