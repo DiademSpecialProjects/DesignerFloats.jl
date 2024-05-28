1f0dcb9232035d671592a015f5f02705302233c9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-05-27 22:45:20 -0400

    Update counts.jl

--- src/qualia/counts.jl
+++ src/qualia/counts.jl
@@ -1,4 +1,20 @@
-# concrete counts
+# counts
+
+n_special_values(::Type{T}) where {T<:UnsignedFloat} = 3 # 0, NaN, Inf
+n_special_values(::Type{T}) where {T<:FiniteUnsignedFloat} = 2 # 0, NaN
+n_special_values(::Type{T}) where {T<:SignedFloat} = 4 # 0, NaN, +/- Inf
+n_special_values(::Type{T}) where {T<:FiniteSignedFloat} = 2 # 0, NaN
+
+n_ordinary_values(::Type{T}) where {T<:BinaryFloat} = n_values(T) - n_special_values(T)
+n_finite_values(::Type{T}) where {T<:BinaryFloat} = n_ordinary_values(T) + 1 # 0
+n_subnormal_values(::Type{T}) where {T<:BinaryFloat} = 2 * n_subnormals(T)
+n_normal_values(::Type{T}) where {T<:BinaryFloat} = n_ordinary_values(T) - n_subnormal_values(T)
+
+n_ordinary_values(x::T) where {T<:BinaryFloat} = n_ordinary_values(T)
+n_finite_values(x::T) where {T<:BinaryFloat} = n_finite_values(T)
+n_subnormal_values(x::T) where {T<:BinaryFloat} = n_subnormal_values(T)
+n_normal_values(x::T) where {T<:BinaryFloat} = n_normal_values(T)
+
 
 CountValues(x::BinaryFLOAT{W,P}) where {W,P} = CountValues(typeof(x))
 CountExponents(x::BinaryFLOAT{W,P}) where {W,P} = CountExponents(typeof(x))
