ac00ea5fe64c74d5082afee2169a337dfceb8ee1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-05-29 07:58:06 -0400

    Update showstring.jl

--- src/provide/showstring.jl
+++ src/provide/showstring.jl
@@ -1,27 +1,27 @@
 const Subscript = ("₁", "₂", "₃", "₄", "₅", "₆", "₇", "₈", "₉",
                    "₁₀", "₁₁", "₁₂", "₁₃", "₁₄", "₁₅", "₁₆")
 
-function Base.string(x::UnsignedFLOAT{W,P}; withnan=true, withinf=true) where {W,P} 
-    specials = string((withnan && nan(x) ? "ᴺ" : ""), (withinf && inf(x) ? "ᴵ" : ""))
+function Base.string(x::UnsignedBinaryFloat{W,P}; withinf=true) where {W,P} 
+    specials = string(withinf && inf(x) ? "ᴵ" : "")
     string("UFloat", W, "p", P, specials)
 end
 
-function Base.string(x::SignedFLOAT{W,P}; withnan=true, withinf=true) where {W,P} 
-    specials = string((withnan && nan(x) ? "ᴺ" : ""), (withinf && inf(x) ? "ᴵ" : ""))
+function Base.string(x::SignedBinaryFloat{W,P}; withinf=true) where {W,P} 
+    specials = string(withinf && inf(x) ? "ᴵ" : "")
     string("SFloat", W, "p", P, specials)
 end
 
-function Base.show(io::IO, x::BinaryFLOAT{W,P}) where {W,P}
+function Base.show(io::IO, x::BinaryFloat{W,P}) where {W,P}
     str = string(x)
     print(io, str)
 end
 
-function pretty(io::IO, x::T;  withnan=true, withinf=true) where {W,P,T<:BinaryFLOAT{W,P}}
+function pretty(io::IO, x::T;  withinf=true) where {W,P,T<:BinaryFloat{W,P}}
     str = string(x; withnan, withinf)
     print(io, str)
 end
 
-function pretty(x::T; withnan=true, withinf=true) where {W,P,T<:BinaryFLOAT{W,P}}
+function pretty(x::T; withinf=true) where {W,P,T<:BinaryFloat{W,P}}
     pretty(stdout, x; withnan, withinf)
 end
 
