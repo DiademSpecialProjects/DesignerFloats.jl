1301d19750bdf0432b3c87204b37b54e5dddb6e1
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-05-30 01:39:27 -0400

    Width==Precision

--- test/genvalues.jl
+++ test/genvalues.jl
@@ -3,12 +3,13 @@ using CSV, Tables, DataFrames, PrettyTables, LaTeXStrings, Latexify
 destpath = realpath(joinpath(s"C:/temp","P3109"))
 
 minwidth = 2
-maxwidth = 12
+maxwidth = 11
 
 results = []
 
 for width in minwidth:maxwidth
   for prec in 1:width-1
+    println("$width $prec")
      xs = SignedFloat{width,prec}
      xu = UnsignedFloat{width,prec}
      fu = FiniteUnsignedFloat{width,prec}
