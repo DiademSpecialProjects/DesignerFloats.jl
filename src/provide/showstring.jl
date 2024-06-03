b05ecfc82036830f8187e722de238dc4c0af74e7
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-06-03 14:50:21 -0400

    hexstring

--- src/provide/showstring.jl
+++ src/provide/showstring.jl
@@ -3,12 +3,12 @@ const Subscript = ("₁", "₂", "₃", "₄", "₅", "₆", "₇", "₈", "₉"
 
 function Base.string(x::UnsignedBinaryFloat{W,P}; withinf=true) where {W,P} 
     specials = string(withinf && has_infinity(x) ? "ᴵ" : "")
-    string("UFloat", W, "p", P, specials, "(",x.value, ", ", x.code, ")")
+    string("UFloat", W, "p", P, specials, "(",x.value, ", ", hexstring(x.code), ")")
 end
 
 function Base.string(x::SignedBinaryFloat{W,P}; withinf=true) where {W,P} 
     specials = string(withinf && has_infinity(x) ? "ᴵ" : "")
-    string("SFloat", W, "p", P, specials, "(",x.value, ", ", x.code, ")")
+    string("SFloat", W, "p", P, specials, "(",x.value, ", ", hexstring(x.code), ")")
 end
 
 function Base.show(io::IO, x::BinaryFloat{W,P}) where {W,P}
@@ -23,5 +23,15 @@ end
 
 function pretty(x::T; withinf=true) where {W,P,T<:BinaryFloat{W,P}}
     pretty(stdout, x; withinf)
+end
+
+const ZeroStr = ["0","00","000","0000","00000","000000","0000000","00000000",]
+
+function hexstring(x::Unsigned)
+    hexstr = string(x, base=16)
+    nhexdigits = length(hexstr)
+    nzeros = max(2, nextpow(2, nhexdigits)) - nhexdigits
+    zs = zerostr[1:nzeros]
+    "0x" * zs * hexstr
 end
 
